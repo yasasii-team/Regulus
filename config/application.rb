1b86ad1ef1cbbc4fbc3543403af0345f4990c2f5
eRy-sk <ride.poke@gmail.com>
2019-04-20 12:52:43 +0900

    Merge pull request #14 from yasasii-team/add-rails-new
    rails new

---
+++ config/application.rb
@@ -0,0 +1,48 @@
+require_relative 'boot'
+
+require 'rails'
+# Pick the frameworks you want:
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
+# require 'rails/test_unit/railtie'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Regulus
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
+
+    config.generators do |g|
+      # rspecの自動生成ファイル
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: true
+    end
+    # Time.zoneの時間を日本時間とする
+    config.time_zone = 'Tokyo'
+    # DBに読み書きする時間をローカルのタイムゾーンで扱う
+    config.active_record.default_timezone = :local
+  end
+end
